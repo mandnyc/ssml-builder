ceabbb8c17bf86534a3d458b162a83bff4e678f6
mmloveaa <mandychan2028@gmail.com>
2017-05-22 10:02:19 -0700

    resolved issue #1

--- index.js
+++ index.js
@@ -204,6 +204,7 @@ Speech.prototype.partOfSpeech = function (options) {
     if (options.role) {
         this._elements.push("<w role=\'" + options.role + "'>" + word + "</w>")
     }
+    return this;
 };
 
 /**
@@ -224,6 +225,7 @@ Speech.prototype.phoneme = function (alphabet, ph, word) {
         ph = ph.replace(/'/g, '&apos;')
     }
     this._elements.push("<phoneme alphabet=\'" + alphabet + "\'" + " ph=\'" + ph + "'>" + escapedWord + "</phoneme>");
+    return this;
 };
 
 /**
